14912518bd72a46389deaf3bef308efe34506f76
freeWillee <will.ik.lee@gmail.com>
2019-05-01 16:39:07 -0600

    Use person.module.css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,13 +4,13 @@
 
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.module.css';
 // We don't need Component because we're not using the class feature that requires Component.
 
 const person = (props) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
