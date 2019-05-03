0bdac9bab1e9f7fe70f67a3e219923d8e3a68050
freeWillee <will.ik.lee@gmail.com>
2019-05-03 13:07:25 -0600

    illustrate propChain Problem

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -50,6 +50,7 @@ const Cockpit = (props) => {
                 onClick={props.clicked}
             >Toggle Persons
             </button>
+            <button onClick={props.login}>Log in</button>
         </div>
     )
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
     };
 
   static getDerivedStateFromProps(props, state) {
@@ -88,6 +89,12 @@ class App extends Component {
     })
   }
 
+  loginHandler = () => {
+    this.setState({
+      authenticated: true
+    })
+  };
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -97,7 +104,9 @@ class App extends Component {
         <Persons 
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler}/>  
+          changed={this.nameChangedHandler}  
+          isAuthenticated={this.state.authenticated}
+        />
       );
 
     }
@@ -113,6 +122,7 @@ class App extends Component {
             showPersons={this.state.showPersons}
             personsLength={this.state.persons.length}
             clicked={this.togglePersonsHandler}
+            login={this.loginHandler}
           />) : null}
           {persons}
         </Aux>
